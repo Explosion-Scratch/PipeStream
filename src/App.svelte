1c8a878429eb44c608bdbacc3aaf093d6247bf5d
Explosion-Scratch <explosion.scratch@gmail.com>
2022-04-19 13:39:24 +0000

    Cool changes

--- src/App.svelte
+++ src/App.svelte
@@ -12,7 +12,21 @@
       plugins: [babel],
     });
   }
-
+  function getObject(key) {
+    if (/^[a-z_][a-z0-9_]+$/i.test(key)) {
+      return `.${key}`;
+    } else {
+      return `[${JSON.stringify(key)}]`;
+    }
+  }
+  function validateJSON(json) {
+    try {
+      JSON.parse(json);
+      return true;
+    } catch (_) {
+      return false;
+    }
+  }
   import { onMount } from "svelte";
   onMount(async () => {
     setInterval(async () => {
@@ -32,6 +46,58 @@
         },
       },
       code: ({ code }) => code,
+    },
+    {
+      type: "fetch",
+      code: ({ method, body, url, output }) => {
+        let opts = "";
+        if (method !== "GET") {
+          let realOpts = { method: method.toUpperCase() };
+          if (body) {
+            if (validateJSON(body)) {
+              realOpts.body = JSON.parse(body);
+            } else {
+              realOpts.body = body;
+            }
+          }
+          opts = `,${JSON.stringify(realOpts)}`;
+        }
+        return `return await fetch(${safe(
+          url
+        )}${opts}).then(response => response.${output}())`;
+      },
+      inputs: {
+        url: {
+          label: "URL to fetch",
+          type: "url",
+          is: "string",
+          default: "http://md5.jsontest.com/?text=example_text",
+        },
+        method: {
+          type: "select",
+          options: ["GET", "POST", "PUT", "PATCH", "DELTE"],
+          default: "GET",
+        },
+        body: {
+          type: "code",
+          language: "json",
+          show: ({ method }) => method !== "GET",
+          isValid: (code) => {
+            try {
+              JSON.parse(code);
+              return true;
+            } catch (_) {
+              return false;
+            }
+          },
+          default: JSON.stringify({}, null, 2),
+        },
+        output: {
+          type: "select",
+          default: "json",
+          options: ["blob", "json", "text", "arrayBuffer"],
+        },
+      },
     },
     {
       type: "translate",
@@ -80,10 +146,7 @@
         .replaceAll("\n", "\\n")
         .replaceAll("`", "\\`")
         .replaceAll("$", "\\$")
-        .replace(
-          /{{(.*?)}}/g,
-          (_, one) => `\$\{CONTEXT[${JSON.stringify(one)}]\}`
-        ) +
+        .replace(/{{(.*?)}}/g, (_, one) => `\$\{CONTEXT${getObject(one)}\}`) +
       "`"
     );
   }
@@ -108,21 +171,17 @@
       if (code.includes("await")) {
         fn = `await (async () => {
 				 ${code}
-			 })().then(setContext)`;
+			 })()`;
       } else if (!code.includes("return")) {
         fn = code;
       } else {
-        fn = `setContext((() => {\n${code}\n})())`;
+        fn = `(() => {\n${code}\n})()`;
       }
       output += `
 			// Step ${parseInt(i) + 1} of ${blocks.length}: ${block.type}
-		 	${fn}\n\n
+		 	CONTEXT${getObject(block.readableId)} = ${fn}\n\n
 		  `;
     }
-    output += `function setContext(newContext){
-		CONTEXT = {...CONTEXT, ...newContext};
-		return CONTEXT;
-	}`;
     return format(output);
   }
   function addBlock() {
@@ -155,6 +214,7 @@
     }
     let out = {
       id: Math.random().toString(36).slice(2),
+      readableId: `step_${blocks.length}`,
       ...block,
       select: block.type,
       inputValues,
@@ -166,7 +226,11 @@
     if (idx < 0) {
       return;
     }
-    blocks[idx] = _block({ id, ...BLOCKLIST.find((i) => i.type === type) });
+    blocks[idx] = _block({
+      id,
+      readableId: blocks[idx].readableId,
+      ...BLOCKLIST.find((i) => i.type === type),
+    });
   }
 </script>
 
@@ -175,50 +239,76 @@
     <div id="noBlocks">No blocks yet, click the "+" button to add one!</div>
   {/if}
   {#each blocks as block}
-    <div id="blockLabel">
-      Step {blocks.findIndex((i) => i.id === block.id) + 1} of {blocks.length}:
-      {block.type}
-    </div>
-    <select
-      bind:value={block.select}
-      on:change={() => switchType(block.id, block.select)}
-    >
-      {#each BLOCKLIST.map((i) => i.type) as type}
-        <option value={type}>
-          {type[0].toUpperCase()}{type.slice(1)}
-        </option>
-      {/each}
-    </select>
-    {#each Object.entries(block.inputs || {}) as [id, input]}
-      <label for={`${block.id}_input`}>{input.label}</label>
-      {#if input.type === "textarea"}
-        <textarea
-          id={`${block.id}_input`}
-          bind:value={block.inputValues[id]}
-          use:autosize
-        />
-      {:else if input.type === "code"}
-        <CodeEditor
-          id={`${block.id}_input`}
-          code={block.inputValues[id]}
-          on:codeUpdate={({ detail: code }) => (
-            (block.inputValues[id] = code.detail), code.detail
-          )}
-        />
-      {:else if input.type === "select"}
-        <select id={`${block.id}_input`} bind:value={block.inputValues[id]}>
-          {#each input.choices || input.options as opt}
-            <option value={opt.value || opt}>{opt.label || opt}</option>
+    <div class="block">
+      <div class="group">
+        <div id="blockLabel">
+          Step {blocks.findIndex((i) => i.id === block.id) + 1} of {blocks.length}:
+          {block.type}
+        </div>
+        <label
+          >ID: <input
+            type="text"
+            bind:value={block.readableId}
+            placeholder="ID"
+          /></label
+        >
+        <select
+          id="blockTypeSelect"
+          bind:value={block.select}
+          on:change={() => switchType(block.id, block.select)}
+        >
+          {#each BLOCKLIST.map((i) => i.type) as type}
+            <option value={type}>
+              {type[0].toUpperCase()}{type.slice(1)}
+            </option>
           {/each}
         </select>
-      {:else}
-        <input
-          type="text"
-          id={`${block.id}_input`}
-          bind:value={block.inputValues[id]}
-        />
-      {/if}
-    {/each}
+      </div>
+      <div class="blockContent">
+        {#if !block.inputs || !Object.entries(block.inputs).length}
+          <span id="noInputs">This block doesn't have any inputs</span>
+        {/if}
+        {#each Object.entries(block.inputs || {}) as [id, input]}
+          <label for={`${block.id}_input`}
+            >{input.label
+              ? input.label
+              : `${id[0].toUpperCase()}${id.slice(1)}`}</label
+          >
+          {#if input.type === "textarea"}
+            <textarea
+              id={`${block.id}_input`}
+              bind:value={block.inputValues[id]}
+              use:autosize
+            />
+          {:else if input.type === "code"}
+            <CodeEditor
+              id={`${block.id}_input`}
+              code={block.inputValues[id]}
+              on:codeUpdate={({ detail: code }) => (
+                (block.inputValues[id] = code.detail), code.detail
+              )}
+            />
+          {:else if input.type === "select"}
+            <select id={`${block.id}_input`} bind:value={block.inputValues[id]}>
+              {#each input.choices || input.options as opt}
+                <option
+                  value={opt.value || opt}
+                  selected={[opt.label, opt, opt.value]
+                    .filter(Boolean)
+                    .includes(input.default)}>{opt.label || opt}</option
+                >
+              {/each}
+            </select>
+          {:else}
+            <input
+              type="text"
+              id={`${block.id}_input`}
+              bind:value={block.inputValues[id]}
+            />
+          {/if}
+        {/each}
+      </div>
+    </div>
   {/each}
   <button id="addBlock" on:click={addBlock}>Add block</button>
   <CodeEditor readonly {code} />
@@ -264,5 +354,77 @@
   button {
     margin-top: 1.5rem;
     .button(@secondary);
+  }
+  // From less style editor I made
+
+  html {
+    padding: 50px 0;
+    height: fit-content;
+    min-height: 100vh;
+  }
+  .block {
+    padding: 20px;
+    margin: 10px 0;
+    box-shadow: 3px 3px 10px #0001;
+    border: 0.15rem dashed #0001;
+    border-radius: 10px;
+    select {
+      background: #0001;
+    }
+    .group {
+      display: flex;
+      align-items: center;
+      position: relative;
+      #blockLabel {
+        font-style: italic;
+        font-size: 1.1rem;
+        opacity: 0.7;
+      }
+      label {
+        display: flex;
+        align-items: center;
+        margin-left: 1rem;
+        opacity: 0.5;
+        &:hover,
+        &:focus,
+        &:focus-within {
+          opacity: 1;
+        }
+        input {
+          height: 100%;
+          margin: 0;
+          margin-left: 0.2rem;
+          width: 5rem;
+        }
+      }
+      padding-bottom: 1rem;
+      margin-bottom: 1rem;
+      border-bottom: 0.15rem dashed #0001;
+      #blockTypeSelect {
+        width: 15%;
+        position: absolute;
+        right: 0.2rem;
+      }
+    }
+    #noInputs {
+      font-size: 1.2rem;
+      font-style: italic;
+      opacity: 0.4;
+    }
+    .blockContent {
+      label,
+      select,
+      input,
+      [id*="code_editor"] {
+        margin: 0.2rem 0.5rem;
+        width: 100%;
+        box-sizing: border-box;
+      }
+      label {
+        margin-top: 20px;
+        font-size: 1.1rem;
+        opacity: 0.6;
+      }
+    }
   }
 </style>
